9ef9435bc85b44ac90c0074643ed60a51d749bb9
ac-marlon <acmarlon@outlook.com>
2017-09-04 20:52:48 -0500

    Falta corregir contador para que comience en 0

--- BinarioDecimalVis.hs
+++ BinarioDecimalVis.hs
@@ -1,9 +1,17 @@
+decBin :: Int -> Int
+decBin a = if a == 0 then 0
+else if a == 1 then 1
+else if div a 2 == 1 && mod a 2 == 0 then 10
+else if div a 2 == 1 && mod a 2 == 1 then 11
+else (mod a 2) + 10 * decBin(div a 2) 
 
-dec:: Int -> Int
-dec a = 
-	if div a 2 == 1 && mod a 2 == 0
-		then 10
-	else if div a 2 == 1 && mod a 2 == 1
-		then 11
-	else
-(mod a 2) + 10 * dec(div a 2) 
+contador :: Int -> Int
+contador 0 = 0
+contador a = 1 + contador(div a 10) 
+
+binDec :: Int -> Int
+binDec a = if a == 0 then 0 
+else if a == 1 then 1
+else if mod a 2 == 0 && div a 2 == 5 then 2
+else if mod a 2 == 1 && div a 2 == 5 then 3
+else (2 ^ ((contador a) * (mod a 10))) + binDec(div a 10)
