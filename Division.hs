4ed3ea17d2030dcf5b0a9145822410405660b1ce
ac-marlon <acmarlon@outlook.com>
2017-09-04 21:47:27 -0500

    Versión Finalizada

--- Division.hs
+++ Division.hs
@@ -1,5 +1,5 @@
-dividir::Int->Int->Int
+dividir :: Int -> Int -> Int
 dividir a 0 = 0
 dividir 0 b = 0
 dividir 0 0 = 0
-dividir a b = if b > a then 0 else dividir (a-b) b + 1
+dividir a b = if b > a then 0 else dividir(a-b) b + 1
