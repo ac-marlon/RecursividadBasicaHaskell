cdb1e5e11686c2d4e3e251d6e4b289f35fcfe865
ac-marlon <acmarlon@outlook.com>
2017-09-12 12:37:10 -0500

    Versión Final

--- BinarioDecimalVis.hs
+++ BinarioDecimalVis.hs
@@ -1,3 +1,5 @@
+--De deciaml a binario
+
 decBin :: Int -> Int
 decBin a = if a == 0 then 0
 else if a == 1 then 1
@@ -5,19 +7,10 @@ else if div a 2 == 1 && mod a 2 == 0 then 10
 else if div a 2 == 1 && mod a 2 == 1 then 11
 else (mod a 2) + 10 * decBin(div a 2) 
 
-contador :: Int -> Int
-contador 0 = 0
-contador a = 1 + contador(div a 10) 
+--De binario a decimal
 
-binDec :: Int -> Int
-binDec a = if a == 0 then 0 
-else if a == 1 then 1
-else if mod a 2 == 0 && div a 2 == 5 then 2
-else if mod a 2 == 1 && div a 2 == 5 then 3
-else (2 ^ ((contador a) * (mod a 10))) + binDec(div a 10)
+binDec : :Int -> Int
+dinDec n = if n < 2 then n
+else mod (n 10) + binDec(div (n 10)) * 2
 
---------------------------------------------------------------------------------------------------------------------------------
 
-binDec::Int->Int
-dinDec n = if n<2 then n
-else mod n 10 + binDec(div n 10)*2
