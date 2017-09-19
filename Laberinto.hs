411f2ae74dfef12816a3c6bf19c6a8d327034da4
DanZaky <30821070+DanZaky@users.noreply.github.com>
2017-09-19 00:21:14 -0500

    Falta todo

--- Laberinto.hs
+++ Laberinto.hs
@@ -1,4 +1,8 @@
-buscar :: [[Char]] -> Int
+buscar::[[String]] -> Int
 buscar [[]] = 0
-buscar x:xs = if x !! 0 == 'x' then 1
- else buscar xs
+buscar (x:xs)=  if x !! 0 == "x" then 0
+                if else x !! 1 == "x" then 1
+                if else x !! 2 == "x" then 2
+                if else x !! 3 == "x" then 3
+                if else x !! 4 == "x" then 4
+		              else buscar xs
