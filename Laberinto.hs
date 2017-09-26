bffb19a4fd6ef93f9e666eab314847a57fc50781
Daniel96114 <dms_1114@hotmail.com>
2017-09-26 11:29:12 -0500

    Se declara matriz y dice que hay en la posición

--- Laberinto.hs
+++ Laberinto.hs
@@ -1,8 +1,13 @@
-buscar::[[String]] -> Int
-buscar [[]] = 0
-buscar (x:xs)=  if x !! 0 == "x" then 0
-                if else x !! 1 == "x" then 1
-                if else x !! 2 == "x" then 2
-                if else x !! 3 == "x" then 3
-                if else x !! 4 == "x" then 4
-		              else buscar xs
+laberinto :: [[Char]]
+laberinto = [ 
+	   ['1','0','F','1','1','1','1'],
+	   ['1','0','1','1','1','1','1'],
+	   ['1','0','0','1','1','1','1'],
+	   ['1','1','X','1','1','1','1'],
+	   ['1','1','1','1','1','1','1']]
+
+
+-- Muestra el elemento en la posicion (fila,columna)
+elemento :: [[Char]]->(Int,Int)->Char
+elemento m (fil,col) =  (m!!fil)!!col
+
