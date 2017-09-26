8509506d9467b058646ee9ba9f330c767e5cd23f
ac-marlon <acmarlon@outlook.com>
2017-09-26 13:59:52 -0500

    Encontrar X

--- Laberinto.hs
+++ Laberinto.hs
@@ -1,3 +1,5 @@
+-- Define el laberinto
+
 laberinto :: [[Char]]
 laberinto = [ 
 	   ['1','0','F','1','1','1','1'],
@@ -6,8 +8,13 @@ laberinto = [
 	   ['1','1','X','1','1','1','1'],
 	   ['1','1','1','1','1','1','1']]
 
+-- Muestra el elemento en la posicion (fila, columna)
 
--- Muestra el elemento en la posicion (fila,columna)
-elemento :: [[Char]]->(Int,Int)->Char
-elemento m (fil,col) =  (m!!fil)!!col
+elemento :: [[Char]] -> (Int, Int) -> Char
+elemento m (fil, col) = (m !! fil) !! col
 
+-- Encuentra la X en la posicion (fila, columna)
+
+buscarX :: [[Char]] -> (Int, Int) -> Char
+buscarX m (fil, col) = if elemento m (fil, col) == 'X' then 'S'
+			else buscarX m (fil, col)
